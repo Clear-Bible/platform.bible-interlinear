283c91567959fc5d68bebe18a7c95343c2ed25ee
Robertson Brinker <robertsonbrinker@gmail.com>
2024-10-11 09:46:08 -0400

    clean up

--- dist_checked_in/assets/descriptions/description-en.md
+++
@@ -1,3 +0,0 @@
-An interlinear for Platform.Bible / Paranext / Paratext 10 Studio.
-
-This extension presents and interlinear view which is generated from alignment data.
--- dist_checked_in/assets/sample.js
+++
@@ -1,6 +0,0 @@
-process.stdin.on('data', (data) => {
-  process.stdout.write(`sample heard main.`);
-  process.stdout.write(`sample heard main say: ${data}`);
-});
-
-process.stdout.write('sample YELLS!');
